13c9bb76cd761e697820975f00d4f755e4b59b0b
lkostoski <lkostoski@york.citycollege.eu>
2025-01-13 13:40:46 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.render('slideshow.ejs');
+});
+
+app.listen(port, ()=>{
+    console.log("Server started on port "+port);
+});
